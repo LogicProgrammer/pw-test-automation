8848f982a99712ec1aee8bc20e85596f3df513fd
LogicProgrammer <56213628+LogicProgrammer@users.noreply.github.com>
2026-03-14 15:32:11 -0400

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,72 @@
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import importPlugin from 'eslint-plugin-import';
+import jsdoc from 'eslint-plugin-jsdoc';
+import tsParser from '@typescript-eslint/parser';
+import globals from 'globals';
+import { fixupPluginRules } from '@eslint/compat';
+import playwright from 'eslint-plugin-playwright';
+
+export default [
+  {
+    ignores: ['node_modules/**']
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,ts,tsx}'],
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      import: fixupPluginRules(importPlugin),
+      jsdoc: fixupPluginRules(jsdoc)
+    },
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        project: './tsconfig.json'
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser
+      }
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true
+        }
+      }
+    },
+    rules: {
+      'no-trailing-spaces': 'warn',
+      'no-empty-pattern': 'off',
+      'no-unused-vars': 'warn',
+      'no-useless-assignment': 'warn',
+      'no-unassigned-vars': 'warn',
+
+      // import rules
+      'import/no-unresolved': 'error',
+      'import/named': 'error',
+      'import/default': 'error',
+      'import/namespace': 'error',
+      'import/no-absolute-path': 'error',
+      'import/no-self-import': 'error',
+
+      'sort-imports': ['error', { ignoreDeclarationSort: true }]
+    }
+  },
+  // playwright plugin rules
+  {
+    ...playwright.configs['flat/recommended'],
+    files: ['tests/**/*.ts'],
+    rules: {
+      ...playwright.configs['flat/recommended'].rules,
+      'playwright/no-useless-await': 'error',
+      'playwright/no-useless-not': 'error',
+      'playwright/no-wait-for-timeout': 'warn',
+      'playwright/missing-playwright-await': 'error',
+      'playwright/consistent-spacing-between-blocks': 'error'
+    }
+  }
+];
---
+++ tests/example.spec.ts
@@ -0,0 +1,10 @@
+import { expect, test } from '@playwright/test';
+
+// FIXME: need to revise this
+test('sample test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('#user-name').fill('standard_user');
+  await page.locator('#password').fill('secret_sauce');
+  await page.locator('#login-button').click();
+  await page.locator('.inventory_item_name').first().click();
+});
